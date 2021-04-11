404c2a901872baacb65ba7b086e6b174bd284fe8
Henrique Vergara <henrique.vergara.hv@gmail.com>
2021-04-11 18:58:12 -0300

    Creating ActiveMQ Consumer from Microservice B

---
+++ microservice-b/src/main/java/com/microservice/microserviceb/routes/ActiveMQReceiverRouter.java
@@ -0,0 +1,13 @@
+package com.microservice.microserviceb.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQReceiverRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("activemq:my-activemq-queue")
+        .to("log:${body}");
+    }
+}
